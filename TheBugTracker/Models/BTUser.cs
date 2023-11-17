51d1a16cb26f130c3d5f1ad105d6f398bc8575e2
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-17 14:16:01 -0600

    Add project files.

---
+++ TheBugTracker/Models/BTUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public required string LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
