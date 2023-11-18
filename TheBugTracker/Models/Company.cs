c5cd64cea7830cce1bd8a3c61eb45b29ecf49024
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-18 12:06:28 -0600

    create Invite model, update BTUser, Company, and Notifications Models.

--- TheBugTracker/Models/Company.cs
+++ TheBugTracker/Models/Company.cs
@@ -1,7 +1,26 @@
-﻿namespace TheBugTracker.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
 {
     public class Company
     {
         public int Id { get; set; }
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+
+        // Navigation Properties -- Creates Relatioship in DB
+
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
+
+        public virtual ICollection<Project> Projects { get; set; } = new HashSet<Project>();
+
+        // Create relationship to invites
+
     }
 }
--- TheBugTracker/Models/Notification.cs
+++ TheBugTracker/Models/Notification.cs
@@ -1,7 +1,49 @@
-﻿namespace TheBugTracker.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
 {
     public class Notification
     {
         public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+
+        [Required]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [Required]
+        [DisplayName("Recipient")]
+        public string RecipientId { get; set; }
+
+        [Required]
+        [DisplayName("Sender")]
+        public string SenderId { get; set; }
+
+        [DisplayName("Has Been Viewed")]
+        public bool Viewed { get; set; }
+
+
+
+        // Navigation Properties -- Creates Relatioship in DB
+
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser Recipient { get; set; }
+
+        public virtual BTUser Sender { get; set; }
+
     }
 }
