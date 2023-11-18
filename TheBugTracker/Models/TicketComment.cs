23ab7614ce0d436633cd7a8c363f56b8a2470862
Rebecca Brietzke <rnbrietzke@outlook.com>
2023-11-18 08:15:58 -0600

    create TicketComment  model

---
+++ TheBugTracker/Models/TicketComment.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Net.Sockets;
+
+namespace TheBugTracker.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+
+        // Navigation Properties -- Creates Relatioship in DB
+
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser User { get; set; }
+
+
+
+    }
+}
